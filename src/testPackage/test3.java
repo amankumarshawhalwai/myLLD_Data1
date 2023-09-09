2cb7bda233abeab8d3ea9ce7d1328b679d7945a2
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-09-09 16:30:52 +0530

    test_3

---
+++ src/testPackage/test3.java
@@ -0,0 +1,17 @@
+package testPackage;
+import java.util.*;
+public class test3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the limit:");
+        int n=sc.nextInt();
+
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        System.out.println("The result is= "+sum);
+        System.out.println("Pushing test3 to git round3");
+    }
+}
