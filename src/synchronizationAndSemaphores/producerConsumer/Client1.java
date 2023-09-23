185bd8a4266149073e71a693202df6d134033f3e
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-09-23 08:10:41 +0530

    Update Client1.java

--- src/synchronizationAndSemaphores/producerConsumer/Client1.java
+++ src/synchronizationAndSemaphores/producerConsumer/Client1.java
@@ -8,10 +8,10 @@ public class Client1 {
         ExecutorService es= Executors.newCachedThreadPool();
         Store1 store1= new Store1(5);//Denotes there are 5 slots for the Producers to fil
 
-        for(int i=1;i<=8;i++){
+        for(int i=1;i<=8;i++){  // There are 8 producers
             es.execute(new Producer1(store1));
         }
-        for(int i=1;i<=20;i++)
+        for(int i=1;i<=20;i++) // There are 20 Consumers. It cam be changed as well
         {
             es.execute(new Consumer1(store1));
         }
