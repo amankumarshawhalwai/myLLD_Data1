e9f3d4cb358ad674b840fc35ac012d0132a04657
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-09-24 11:43:03 +0530

    generics_v1.2

---
+++ src/generics/Main2.java
@@ -0,0 +1,19 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Main2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr1= new ArrayList<>();
+
+        ArrayList<Objects> arr2= new ArrayList<>();
+
+        /**
+         * This shows whether we declare a List of type INTEGER or OBJECTS at the run time
+         * it is just identified as a LIST. This can be seen from line 15 and line 16
+         */
+        System.out.println(arr1.getClass().getName());
+        System.out.println(arr1.getClass()==arr2.getClass());
+    }
+}
