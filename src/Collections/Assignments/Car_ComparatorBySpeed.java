5eddb213819fe707a95d004addc039f0cf37539c
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-09-26 09:58:39 +0530

    collections_v2.1.2

--- src/Collections/Assignments/Car_ComparatorBySpeed.java
+++ src/Collections/Assignments/Car_ComparatorBySpeed.java
@@ -1,10 +1,13 @@
 package Collections.Assignments;
 
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.Vector;
 
 public class Car_ComparatorBySpeed implements Comparator<CarDataForComparator> {
     @Override
     public int compare(CarDataForComparator o1, CarDataForComparator o2) {
-        return Integer.compare(o1.speed,o2.speed);
+        return Integer.compare(o1.speed, o2.speed);
     }
 }
--- src/Collections/Main_Comparable.java
+++ src/Collections/Main_Comparable.java
@@ -1,8 +1,6 @@
 package Collections;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main_Comparable {
 
