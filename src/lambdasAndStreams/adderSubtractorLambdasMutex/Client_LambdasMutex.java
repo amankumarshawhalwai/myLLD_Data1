2f64d765dcc3d2f0ff72eba83f4a5b7301072b2e
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-10-02 11:04:36 +0530

    lambdas_v1.0.1

--- src/lambdasAndStreams/adderSubtractorLambdasMutex/Client_LambdasMutex.java
+++ src/lambdasAndStreams/adderSubtractorLambdasMutex/Client_LambdasMutex.java
@@ -9,6 +9,13 @@ public class Client_LambdasMutex {
         Value_LambdasMutex v=new Value_LambdasMutex();
         Lock l= new ReentrantLock();
 
+        /**
+         * Working of Lambda Expressions
+         * step 1: Creates an anonymous class of adderLambMutex
+         * step 2: It will implement Callable<Void>
+         * step 3: It will pass all the variables from the current scope to class
+         * step 4: It will copy all the code from the lambda body to the interface method
+         */
         Callable<Void> adderLambMutex= ()->{
             for(int i=1;i<=5000;i++)
             {
