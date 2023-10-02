ee8f89c8ea89ca82387311e659b808b22f37e44d
Aman Kumar Shaw Halwai <amanshaw616@gmail.com>
2023-10-02 15:51:05 +0530

    lambdas_v1.1

---
+++ src/lambdasAndStreams/comparatorLambda/Client_ComparatorLambda.java
@@ -0,0 +1,32 @@
+package lambdasAndStreams.comparatorLambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Client_ComparatorLambda {
+    public static void main(String[] args) {
+        ArrayList<Student_Lambda> arr= new ArrayList<>();
+
+        arr.add(new Student_Lambda(12,"Aryan",98.5));
+        arr.add(new Student_Lambda(13,"Aman",98.6));
+
+        //VERSION 1
+        /*Comparator<Student_Lambda> studentComparatorByMarks= (o1,o2)->{
+            if(o1.name.compareTo(o2.name)<0)
+                return -1;
+            if(o1.name.equals(o2.name))
+                return 1;
+            return 0;
+        };*/
+        //VERSION 2
+        Collections.sort(arr,(o1,o2)->{
+            if(o1.marks<o2.marks) return -1;
+            if(o1.marks>o2.marks) return 1;
+            return 0;
+        });
+        for(Student_Lambda s1:arr)
+            System.out.println(s1);
+
+    }
+}
---
+++ src/lambdasAndStreams/comparatorLambda/Student_Lambda.java
@@ -0,0 +1,23 @@
+package lambdasAndStreams.comparatorLambda;
+
+public class Student_Lambda {
+    public int roll;
+    public String name;
+    double marks;
+
+    public Student_Lambda(int roll,String name,double marks)
+    {
+        this.roll=roll;
+        this.name=name;
+        this.marks=marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student_Lambda{" +
+                "roll=" + roll +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
